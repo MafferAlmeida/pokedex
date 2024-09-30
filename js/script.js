d39591854fb502177dbb95cfc8043fbec30d9256
MafferAlmeida <mfotografia92@gmail.com>
2024-09-30 18:51:26 -0300

    js atualizado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const pokemonName= document.querySelector('.pokemon__name');
 const pokemonNumber= document.querySelector('.pokemon__number');
 const pokemonImage= document.querySelector('.pokemon__image');
+const form= document.querySelector('.form');
+const input= document.querySelector('.input__search');
+
 
 const fetchPokemon = async (pokemon) => {
     const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
@@ -11,10 +14,18 @@ const fetchPokemon = async (pokemon) => {
 }
 
 const renderPokemon = async (pokemon) => {
+
     const data = await fetchPokemon(pokemon);
+
     pokemonName.innerHTML = data.name;
     pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny']
+    pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_shiny'];
 
 }
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    renderPokemon(input.value);
+});
 
