e2a65bafc3d4018be2945ab3a9ca0a539087d0d7
MafferAlmeida <mfotografia92@gmail.com>
2024-09-30 17:50:47 -0300

    renderizar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const pokemonName= document.querySelector('.pokemon__name');
+
 const fetchPokemon = async (pokemon) => {
     const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
 
@@ -6,3 +8,9 @@ const fetchPokemon = async (pokemon) => {
     return data;
 }
 
+const renderPokemon = async (pokemon) => {
+    const data = await fetchPokemon(pokemon);
+    pokemonName.innerHTML = data.name;
+}
+
+renderPokemon('31')
